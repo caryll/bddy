c609815cfc41af3785ce5379ae0f5617827d9fc3
be5invis <belleve@typeof.net>
2017-05-26 16:19:23 +0800

    simpliest framework

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const messages = require('./lib/messages');
-console.log(messages.DIAMOND, "Work in progress");
+const bddy = require('./lib/engine');
+const defs = new bddy.Definitions();
+const { file } = require('./lib/filepath');
+
+defs.forall('src/*.js', async function (target) {
+	await this.command('echo', 'source', target)
+});
+defs.forall('test/*.js', async function (target) {
+	await this.need(file(`src/${target.name}.js`))
+	await this.need(file(`src/${target.name}.js`))
+	await this.command('echo', 'hello', target)
+});
+
+defs.run(file('test/a.js'), defs.createContext())
