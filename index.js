17a46f0cdda2d4da80aa64b84f6ae36d0942801d
be5invis <belleve@typeof.net>
2017-05-26 19:11:11 +0800

    add REAL index.js

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const bddy = require('./lib/engine');
-const defs = new bddy.Definitions();
+const engine = require('./lib/engine');
 const { file } = require('./lib/filepath');
 
-defs.forall(`test/b.txt`, async function (target) {
-	let prerequisites = await this.need(file(`test/a.txt`));
-	await this.command('cp', prerequisites[0], target);
-})
-
-defs.forall(`test/c.txt`, async function (target) {
-	let prerequisites = await this.need(file(`test/b.txt`));
-	await this.command('cp', prerequisites[0], target);
-})
-
-defs.forall(`test/d.txt`, async function (target) {
-	let prerequisites = await this.need(file(`test/b.txt`));
-	await this.command('cp', prerequisites[0], target);
-})
-
-defs.forall(`test/e.txt`, async function (target) {
-	let prerequisites = await this.need(file(`test/c.txt`), file(`test/d.txt`));
-	await this.command('cp', prerequisites[0], target);
-})
-
-defs.run(file('test/e.txt'), defs.createContext()).catch(function (ex) { console.log(ex) })
+exports.def = function (F) {
+	let defs = new engine.Definitions;
+	F.call(defs, defs);
+	return defs;
+}
+exports.run = function (defs, target) {
+	return defs.run(target, defs.createContext()).catch(function (ex) { console.log(ex) });
+}
+exports.file = file;
---
+++ test/sample1.js
@@ -0,0 +1,31 @@
+const { def, run, file } = require('../index');
+const fs = require('fs-extra')
+
+const dir = 'test/sample1'
+
+const recipe = def(function () {
+	this.forall(`${dir}/b.txt`, async function (target) {
+		let prerequisites = await this.need(file(`${dir}/a.txt`));
+		await this.command('cp', prerequisites[0], target);
+	});
+
+	this.forall(`${dir}/c.txt`, async function (target) {
+		let prerequisites = await this.need(file(`${dir}/b.txt`));
+		await this.command('cp', prerequisites[0], target);
+	});
+
+	this.forall(`${dir}/d.txt`, async function (target) {
+		let prerequisites = await this.need(file(`${dir}/b.txt`));
+		await this.command('cp', prerequisites[0], target);
+	});
+
+	this.forall(`${dir}/e.txt`, async function (target) {
+		let [$1, $2] = await this.need(file(`${dir}/c.txt`), file(`${dir}/d.txt`));
+		let txt1 = await fs.readFile(`${$1}`, 'utf-8');
+		let txt2 = await fs.readFile(`${$2}`, 'utf-8');
+		console.log(txt1, txt2);
+		await fs.writeFile(`${target}`, txt1 + '\n\n' + txt2);
+	});
+});
+
+run(recipe, file(`${dir}/e.txt`));
