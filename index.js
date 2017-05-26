50914903d3ada2d3015f33899a3948dcffd96579
be5invis <belleve@typeof.net>
2017-05-26 16:48:31 +0800

    add parallism

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const defs = new bddy.Definitions();
 const { file } = require('./lib/filepath');
 
 defs.forall('src/*.js', async function (target) {
-	await this.command('echo', 'source', target)
+	await this.command('echo', 'source', target);
+	await this.command('sleep', 2);
 });
 defs.forall('test/*.js', async function (target) {
-	await this.need(file(`src/${target.name}.js`))
+	await this.need(
+		file(`src/${target.name}.js`),
+		file(`src/${target.name}.2.js`),
+		file(`src/${target.name}.3.js`),
+		file(`src/${target.name}.4.js`),
+		file(`src/${target.name}.5.js`),
+		file(`src/${target.name}.6.js`),
+		file(`src/${target.name}.7.js`),
+		file(`src/${target.name}.8.js`),
+		file(`src/${target.name}.9.js`)
+	)
 	await this.need(file(`src/${target.name}.js`))
 	await this.command('echo', 'hello', target)
 });
