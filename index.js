6bb3944fd6b02c365fc9db4204d895c118083222
be5invis <belleve@typeof.net>
2017-05-29 20:16:25 +0800

    Add plugin and cotarget system

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const engine = require('./lib/engine');
 const { file } = require('./lib/targets/filepath');
 const { virt } = require('./lib/targets/virtual');
+const { anyfile } = require('./lib/cotargets/filepattern.js');
+const { anyvirtual } = require('./lib/cotargets/anyvirtual.js');
 
-exports.def = function (F) {
-	let defs = new engine.Definitions;
-	F.call(defs, defs);
-	return defs;
-}
-exports.run = function (defs, target) {
-	return defs.run(target, defs.createContext()).catch(function (ex) { console.log(ex) });
-}
+exports.bddy = function () { return new engine.Context }
 exports.file = file;
 exports.virt = virt;
+exports.anyfile = anyfile;
+exports.anyvirtual = anyvirtual;
--- test/sample1.js
+++ test/sample1.js
@@ -1,29 +1,30 @@
-const { def, run, file, virt } = require('../index');
+const { bddy, file, virt, anyfile, anyvirtual } = require('../index');
+const Command = require('../lib/plugins/command');
 const fs = require('fs-extra')
 
 const dir = file(`${__dirname}/sample1`);
 
-const recipe = def(function () {
-	this.forall(`${dir}/b.txt`, async function (target) {
+bddy().define(function () {
+	this.for(anyfile(`${dir}/b.txt`), async function (target) {
 		let prerequisites = await this.need(file(`${dir}/a.txt`));
 		await this.command('cp', prerequisites[0], target);
 	});
 
-	this.forall(`${dir}/c.txt`, async function (target) {
+	this.for(anyfile(`${dir}/c.txt`), async function (target) {
 		let prerequisites = await this.need(file(`${dir}/b.txt`));
 		await this.command('cp', prerequisites[0], target);
 	});
 
-	this.forall('knot', async function (target) {
+	this.for(anyvirtual('knot'), async function (target) {
 		await this.need(file(`${dir}/b.txt`));
 	});
 
-	this.forall(`${dir}/d.txt`, async function (target) {
+	this.for(anyfile(`${dir}/d.txt`), async function (target) {
 		let prerequisites = await this.need(virt('knot'));
 		await this.command('cp', `${dir}/b.txt`, target);
 	});
 
-	this.forall(`${dir}/e.txt`, async function (target) {
+	this.for(anyfile(`${dir}/e.txt`), async function (target) {
 		let [$1, $2] = await this.need(file(`${dir}/c.txt`), file(`${dir}/d.txt`));
 		let txt1 = await fs.readFile(`${$1}`, 'utf-8');
 		let txt2 = await fs.readFile(`${$2}`, 'utf-8');
@@ -31,6 +32,4 @@ const recipe = def(function () {
 		await fs.writeFile(`${target}`, txt1 + '\n\n' + txt2);
 		this.message(`${target} written.`)
 	});
-});
-
-run(recipe, file(`${dir}/e.txt`));
+}).loadPlugin(new Command).wish(file(`${dir}/e.txt`));
