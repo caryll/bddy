af4aa0a36180a08d21e323b63cf6458ba9331444
be5invis <belleve@typeof.net>
2017-05-29 20:38:26 +0800

    add createTargetFromString into CoTarget

--- test/sample1.js
+++ test/sample1.js
@@ -6,30 +6,30 @@ const dir = file(`${__dirname}/sample1`);
 
 bddy().define(function () {
 	this.for(anyfile(`${dir}/b.txt`), async function (target) {
-		let prerequisites = await this.need(file(`${dir}/a.txt`));
+		let prerequisites = await this.need(`${dir}/a.txt`);
 		await this.command('cp', prerequisites[0], target);
 	});
 
 	this.for(anyfile(`${dir}/c.txt`), async function (target) {
-		let prerequisites = await this.need(file(`${dir}/b.txt`));
+		let prerequisites = await this.need(`${dir}/b.txt`);
 		await this.command('cp', prerequisites[0], target);
 	});
 
 	this.for(anyvirtual('knot'), async function (target) {
-		await this.need(file(`${dir}/b.txt`));
+		await this.need(`${dir}/b.txt`);
 	});
 
 	this.for(anyfile(`${dir}/d.txt`), async function (target) {
-		let prerequisites = await this.need(virt('knot'));
+		let prerequisites = await this.need('knot');
 		await this.command('cp', `${dir}/b.txt`, target);
 	});
 
 	this.for(anyfile(`${dir}/e.txt`), async function (target) {
-		let [$1, $2] = await this.need(file(`${dir}/c.txt`), file(`${dir}/d.txt`));
+		let [$1, $2] = await this.need(`${dir}/c.txt`, `${dir}/d.txt`);
 		let txt1 = await fs.readFile(`${$1}`, 'utf-8');
 		let txt2 = await fs.readFile(`${$2}`, 'utf-8');
 		console.log(txt1, txt2);
 		await fs.writeFile(`${target}`, txt1 + '\n\n' + txt2);
 		this.message(`${target} written.`)
 	});
-}).loadPlugin(new Command).wish(file(`${dir}/e.txt`));
+}).loadPlugin(new Command).wish(`${dir}/e.txt`);
