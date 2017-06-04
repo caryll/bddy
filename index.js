59aebebbd19569e326db6b41c3428df437b0a980
be5invis <belleve@typeof.net>
2017-06-04 17:49:15 +0800

    make them pretty

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const engine = require('./lib/engine');
-const { file } = require('./lib/targets/filepath');
-const { virt } = require('./lib/targets/virtual');
-const { anyfile } = require('./lib/cotargets/filepattern.js');
-const { anyvirtual } = require('./lib/cotargets/anyvirtual.js');
+const engine = require("./lib/engine");
+const { file } = require("./lib/targets/filepath");
+const { virt } = require("./lib/targets/virtual");
+const { anyfile } = require("./lib/cotargets/filepattern.js");
+const { anyvirtual } = require("./lib/cotargets/anyvirtual.js");
 
-exports.bddy = function () { return new engine.Context }
+exports.bddy = function() {
+	return new engine.Context();
+};
 exports.file = file;
 exports.virt = virt;
 exports.anyfile = anyfile;
