347d24f75dc131afe74694ac3f5bcdfb1028c7aa
be5invis <belleve@typeof.net>
2017-06-20 18:50:54 +0800

    update sample

--- test/sample1.js
+++ test/sample1.js
@@ -3,27 +3,27 @@ const fs = require("fs-extra");
 
 const dir = file(`${__dirname}/sample1`);
 
-bddy(function(ctx, def) {
-	def(any.file(`${dir}/b.txt`), async function(target) {
+bddy(function(ctx, forany) {
+	forany.file(`${dir}/b.txt`).def(async function(target) {
 		let prerequisites = await this.need(`${dir}/a.txt`);
 		await this.run("cp", prerequisites[0], target);
 	});
 
-	def(any.file(`${dir}/c.txt`), async function(target) {
+	forany.file(`${dir}/c.txt`).def(async function(target) {
 		let prerequisites = await this.need(`${dir}/b.txt`);
 		await this.run("cp", prerequisites[0], target);
 	});
 
-	def(any.virt("knot"), async function(target) {
+	forany.virt("knot").def(async function(target) {
 		await this.need(`${dir}/b.txt`);
 	});
 
-	def(any.file(`${dir}/d.txt`), async function(target) {
+	forany.file(`${dir}/d.txt`).def(async function(target) {
 		let prerequisites = await this.need("knot");
 		await this.run("cp", `${dir}/b.txt`, target);
 	});
 
-	def(any.file(`${dir}/e.txt`), async function(target) {
+	forany.file(`${dir}/e.txt`).def(async function(target) {
 		let [$1, $2] = await this.need(`${dir}/c.txt`, `${dir}/d.txt`);
 		let txt1 = await fs.readFile(`${$1}`, "utf-8");
 		let txt2 = await fs.readFile(`${$2}`, "utf-8");
@@ -32,9 +32,9 @@ bddy(function(ctx, def) {
 		this.message(`${target} written.`);
 	});
 
-	def(any.file(`${dir}/subdir`), ctx.ensureDir);
+	forany.file(`${dir}/subdir`).def(ctx.ensureDir);
 
-	def(any.file(`${dir}/subdir/f.txt`), async function(target) {
+	forany.file(`${dir}/subdir/f.txt`).def(async function(target) {
 		let [$1, $2] = await this.need(`${dir}/subdir`, `${dir}/e.txt`);
 		await this.run("cp", `${$2}`, `${$1}/f.txt`);
 	});
