0fe92386f0e9d29c1d88ad8948c719db5382b796
be5invis <belleve@typeof.net>
2017-06-23 21:12:30 +0800

    show errors in a prettier outfit

--- bin/bddy.js
+++ bin/bddy.js
@@ -2,6 +2,25 @@
 
 "use strict";
 
+var PrettyError = require("pretty-error");
+var pe = new PrettyError();
+
+pe.appendStyle({
+	"pretty-error > header > title > kind": { display: "none" },
+	"pretty-error > header > colon": { display: "none" },
+	"pretty-error > header > message": {
+		color: "bright-white",
+		background: "cyan",
+		padding: "0 0"
+	},
+	"pretty-error > trace > item": { marginLeft: 2, bullet: '"<grey>-</grey>"' },
+	"pretty-error > trace > item > header > pointer > file": { color: "bright-cyan" },
+	"pretty-error > trace > item > header > pointer > colon": { color: "cyan" },
+	"pretty-error > trace > item > header > pointer > line": { color: "bright-cyan" },
+	"pretty-error > trace > item > header > what": { color: "bright-white" },
+	"pretty-error > trace > item > footer > addr": { display: "none" }
+});
+
 const argv = require("yargs").argv;
 
 const path = require("path");
@@ -19,4 +38,8 @@ async function build(defs, argv) {
 }
 
 const bddydef = require(path.join(process.cwd(), "bddy.js"));
-build(bddydef, argv);
+build(bddydef, argv).catch(function(e) {
+	var renderedError = pe.render(e);
+	console.log(renderedError);
+	process.exit(1);
+});
